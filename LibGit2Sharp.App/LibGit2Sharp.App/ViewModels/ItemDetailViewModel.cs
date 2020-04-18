f32f0f48304d3e38baa80d07c1fc8a4090e49824
Roger Ye <shugaoye@yahoo.com>
2020-04-18 23:30:42 +0800

    Added LibGit2Sharp.App.

---
+++ LibGit2Sharp.App/LibGit2Sharp.App/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using LibGit2Sharp.App.Models;
+
+namespace LibGit2Sharp.App.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
